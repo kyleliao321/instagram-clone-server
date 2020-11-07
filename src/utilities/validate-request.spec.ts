8d859252addda5c61650f76b0555a78d7da36ae1
kyleliao321 <kyleliao032@gmail.com>
2020-11-08 00:01:31 +0800

    add register-schema and validate-request utility && finish basic unit test

---
+++ src/utilities/validate-request.spec.ts
@@ -0,0 +1,60 @@
+import * as Joi from 'joi';
+import makeValidateRequest from './validate-request';
+import { NextFunction, Request, Response } from "express";
+
+describe('validate request', () => {
+    test('should trigger next function when schema validation succeed', () => {
+        // given
+        const mockRequest = {} as Request;
+
+        const mockResponse = {} as Response;
+
+        const mockNextFunction = jest.fn();
+
+        const mockValidateResult = {
+            error: undefined
+        } as Joi.ValidationResult;
+
+        const mockSchema = {
+            validate: (value: any) => mockValidateResult
+        } as Joi.Schema;
+
+        const validateRequest = makeValidateRequest({ schema: mockSchema });
+        
+        // when
+        validateRequest(mockRequest, mockResponse, mockNextFunction);
+
+        // expect
+        expect(mockNextFunction).toHaveBeenCalled();
+    })
+
+    test('should not trigger next function when schema validation failed', () => {
+        // given
+        const mockRequest = {} as Request;
+
+        const mockResponse = {
+            sendStatus: (code: number) => {},
+            send: (body?: any) => {}
+        } as Response;
+
+        const mockNextFunction = jest.fn();
+
+        const mockValidationError = {} as Joi.ValidationError;
+
+        const mockValidateResult = {
+            error: mockValidationError
+        } as Joi.ValidationResult;
+
+        const mockSchema = {
+            validate: (value: any) => mockValidateResult
+        } as Joi.Schema;
+
+        const validateRequest = makeValidateRequest({ schema: mockSchema });
+        
+        // when
+        validateRequest(mockRequest, mockResponse, mockNextFunction);
+
+        // expect
+        expect(mockNextFunction).toHaveBeenCalledTimes(0);
+    })
+})
---
+++ src/utilities/validate-request.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { Schema } from "joi";
+
+export default function makeValidateRequest(dependency: {
+    schema: Schema
+}) {
+    return function validateRequest(req: Request, res: Response, next: NextFunction): void {
+
+        const { error } = dependency.schema.validate(req.body);
+
+        if (error) {
+            res.sendStatus(500);
+            res.send('Invalid request');
+        } else {
+            next();
+        }   
+    }
+}
