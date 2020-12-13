c00fc1654fbd3a3c35d92a59897a5eb96ba545a7
kyleliao321 <kyleliao032@gmail.com>
2020-12-13 14:19:20 +0800

    add type-support for knex table

--- db/__test__/migrations.spec.ts
+++ db/__test__/migrations.spec.ts
@@ -1,4 +1,10 @@
-import { AccountsTable, PostsTable, UserPostRelationsTable, UserRelationsTable, UsersTable } from '../constants';
+import {
+  AccountsTable,
+  PostsTable,
+  UserPostRelationsTable,
+  UserRelationsTable,
+  UsersTable
+} from '../constants';
 import db from '../index';
 
 describe('migration', () => {
@@ -51,37 +57,101 @@ describe('migration', () => {
   });
 
   test('users_table should have correct structure', async () => {
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.userId)).toBe(true);
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.userName)).toBe(true);
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.description)).toBe(true);
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.imageSrc)).toBe(true);
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.postNum)).toBe(true);
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.followerNum)).toBe(true);
-    expect(await db.schema.hasColumn(UsersTable.name, UsersTable.columns.followingNum)).toBe(true);
+    expect(
+      await db.schema.hasColumn(UsersTable.name, UsersTable.columns.userId)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(UsersTable.name, UsersTable.columns.userName)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(UsersTable.name, UsersTable.columns.description)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(UsersTable.name, UsersTable.columns.imageSrc)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(UsersTable.name, UsersTable.columns.postNum)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(UsersTable.name, UsersTable.columns.followerNum)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        UsersTable.name,
+        UsersTable.columns.followingNum
+      )
+    ).toBe(true);
   });
 
   test('accounts_table should have correct structure', async () => {
-    expect(await db.schema.hasColumn(AccountsTable.name, AccountsTable.columns.userId)).toBe(true);
-    expect(await db.schema.hasColumn(AccountsTable.name, AccountsTable.columns.userName)).toBe(true);
-    expect(await db.schema.hasColumn(AccountsTable.name, AccountsTable.columns.hashedPassword)).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        AccountsTable.name,
+        AccountsTable.columns.userId
+      )
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        AccountsTable.name,
+        AccountsTable.columns.userName
+      )
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        AccountsTable.name,
+        AccountsTable.columns.hashedPassword
+      )
+    ).toBe(true);
   });
 
   test('posts_table should have correct structure', async () => {
-    expect(await db.schema.hasColumn(PostsTable.name, PostsTable.columns.postId)).toBe(true);
-    expect(await db.schema.hasColumn(PostsTable.name, PostsTable.columns.description)).toBe(true);
-    expect(await db.schema.hasColumn(PostsTable.name, PostsTable.columns.location)).toBe(true);
-    expect(await db.schema.hasColumn(PostsTable.name, PostsTable.columns.createdAt)).toBe(true);
-    expect(await db.schema.hasColumn(PostsTable.name, PostsTable.columns.imageSrc)).toBe(true);
-    expect(await db.schema.hasColumn(PostsTable.name, PostsTable.columns.postedUser)).toBe(true);
+    expect(
+      await db.schema.hasColumn(PostsTable.name, PostsTable.columns.postId)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(PostsTable.name, PostsTable.columns.description)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(PostsTable.name, PostsTable.columns.location)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(PostsTable.name, PostsTable.columns.createdAt)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(PostsTable.name, PostsTable.columns.imageSrc)
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(PostsTable.name, PostsTable.columns.postedUser)
+    ).toBe(true);
   });
 
   test('user_relations_table should have correct structure', async () => {
-    expect(await db.schema.hasColumn(UserRelationsTable.name, UserRelationsTable.columns.followerId)).toBe(true);
-    expect(await db.schema.hasColumn(UserRelationsTable.name, UserRelationsTable.columns.followingId)).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        UserRelationsTable.name,
+        UserRelationsTable.columns.followerId
+      )
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        UserRelationsTable.name,
+        UserRelationsTable.columns.followingId
+      )
+    ).toBe(true);
   });
 
   test('user_post_relations_table should have correct structure', async () => {
-    expect(await db.schema.hasColumn(UserPostRelationsTable.name, UserPostRelationsTable.columns.userId)).toBe(true);
-    expect(await db.schema.hasColumn(UserPostRelationsTable.name, UserPostRelationsTable.columns.postId)).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        UserPostRelationsTable.name,
+        UserPostRelationsTable.columns.userId
+      )
+    ).toBe(true);
+    expect(
+      await db.schema.hasColumn(
+        UserPostRelationsTable.name,
+        UserPostRelationsTable.columns.postId
+      )
+    ).toBe(true);
   });
 });
---
+++ db/types.ts
@@ -0,0 +1,70 @@
+import Knex from 'knex';
+
+declare module 'knex/types/tables' {
+  interface User {
+    user_id: string;
+    user_name: string;
+    alias: string;
+    description: string | null;
+    imageSrc: string | null;
+    post_num: number;
+    follower_num: number;
+    following_num: number;
+  }
+
+  interface Account {
+    user_id: string;
+    user_name: string;
+    hashed_password: string;
+  }
+
+  interface Post {
+    post_id: string;
+    location: string | null;
+    description: string | null;
+    created_at: string;
+    image_src: string;
+    posted_id: string;
+  }
+
+  interface UserRelation {
+    follower_id: string;
+    following_id: string;
+  }
+
+  interface UserPostRelation {
+    user_id: string;
+    post_id: string;
+  }
+
+  interface Tables {
+    users: User;
+    users_table: Knex.CompositeTableType<
+      User,
+      Pick<User, 'user_id' | 'user_name' | 'alias'> &
+        Partial<Omit<User, 'user_id' | 'user_name' | 'alias'>>,
+      Partial<Omit<User, 'user_id'>>
+    >;
+
+    accounts: Account;
+    accounts_table: Knex.CompositeTableType<
+      Account,
+      Account,
+      Partial<Omit<Account, 'user_id'>>
+    >;
+
+    posts: Post;
+    posts_table: Knex.CompositeTableType<
+      Post,
+      Pick<Post, 'post_id'|'image_src'|'created_at'|'posted_id'> &
+        Partial<Pick<Post, 'location'|'description'>>,
+      Partial<Omit<Post, 'post_id'>>
+    >;
+
+    user_relations: UserRelation;
+    user_relations_table: Knex.CompositeTableType<UserRelation>;
+
+    user_post_relations: UserPostRelation;
+    user_post_relations_table: Knex.CompositeTableType<UserPostRelation>;
+  }
+}
